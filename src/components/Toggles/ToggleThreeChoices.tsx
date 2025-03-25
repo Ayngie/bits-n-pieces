7dda8051f0dca9c450179c13f0dd6ab6faf6a8b7
Ayngie <angelicareutersward@gmail.com>
2025-03-25 15:59:33 +0100

    Feat: created component for future development

---
+++ src/components/Toggles/ToggleThreeChoices.tsx
@@ -0,0 +1,11 @@
+const ToggleThreeChoices = () => {
+  return (
+    <div className="card">
+      <h2>Toggle three choices!</h2>
+
+      {/* TODO: implement */}
+    </div>
+  );
+};
+
+export default ToggleThreeChoices;
