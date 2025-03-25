31a6f9d96cb24a54bebd70a0b03947989f329094
Ayngie <angelicareutersward@gmail.com>
2025-03-25 16:02:08 +0100

    Feat: added className + TODO, updated file structure

--- src/components/Toggles/Toggles.tsx
+++ src/components/Toggles/Toggles.tsx
@@ -4,11 +4,12 @@ import ToggleTheme from './ToggleTheme';
 
 const Toggles: React.FC = () => {
   return (
-    <div className="card">
+    <div className="card bordered-section">
       <h3>Toggles:</h3>
       <Toggle />
       <ToggleColored />
       <ToggleTheme />
+      {/* TODO: incorporate the rest? */}
     </div>
   );
 };
