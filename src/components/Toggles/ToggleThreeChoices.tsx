ec3e1dd95b3bec8373d84739e8f241b2d0647553
Ayngie <angelicareutersward@gmail.com>
2025-03-25 16:23:00 +0100

    Feat: added toggle between three choices

--- src/components/Toggles/ToggleThreeChoices.tsx
+++ src/components/Toggles/ToggleThreeChoices.tsx
@@ -1,9 +1,30 @@
-const ToggleThreeChoices = () => {
+import { JSX, useState } from 'react';
+import ComponentMappingWithTypes from '../DynamicComponentMapping/ComponentMappingWithTypes';
+import ComponentMappingWithInterfaceAndArray from '../DynamicComponentMapping/ComponentMappingWithInterfaceAndArray';
+
+const ToggleThreeChoices: React.FC = () => {
+  const [mappingIndex, setMappingIndex] = useState<number>(0);
+
+  // Function to cycle through states (0->1->2->0)
+  const switchMapping = (): void => {
+    setMappingIndex((prevIndex) => (prevIndex + 1) % 3);
+  };
+
+  // Array of components
+  const components: JSX.Element[] = [
+    <div key="neutral">
+      <p>Neutral choice</p>
+    </div>,
+    <ComponentMappingWithTypes key="types" />,
+    <ComponentMappingWithInterfaceAndArray key="interface_and_array" />,
+  ];
+
   return (
     <div className="card">
       <h2>Toggle three choices!</h2>
+      <button onClick={switchMapping}>Switch choice</button>
 
-      {/* TODO: implement */}
+      {components[mappingIndex]}
     </div>
   );
 };
