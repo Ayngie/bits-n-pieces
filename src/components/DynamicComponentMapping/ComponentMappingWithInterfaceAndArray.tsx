3e356f2171fd60fef5c54f1cb035d3d1097ba98c
Ayngie <angelicareutersward@gmail.com>
2025-03-25 16:00:06 +0100

    Feat: created component for future development

---
+++ src/components/DynamicComponentMapping/ComponentMappingWithInterfaceAndArray.tsx
@@ -0,0 +1,9 @@
+const ComponentMappingWithInterfaceAndArray = () => {
+  return (
+    <div>
+      <h2>Component Mapping with Interface and Array</h2>
+      {/* TODO: finish coding this */}
+    </div>
+  );
+};
+export default ComponentMappingWithInterfaceAndArray;
