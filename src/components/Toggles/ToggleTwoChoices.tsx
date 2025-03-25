4fc8d24bd3efa0f087008c49f95ca02d6314fbdf
Ayngie <angelicareutersward@gmail.com>
2025-03-25 16:05:53 +0100

    Feat: new component for toggling choices

---
+++ src/components/Toggles/ToggleTwoChoices.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import ToggleWithProps from './ToggleWithProps';
+import ComponentMappingWithTypes from '../DynamicComponentMapping/ComponentMappingWithTypes';
+import ComponentMappingWithInterfaceAndArray from '../DynamicComponentMapping/ComponentMappingWithInterfaceAndArray';
+
+const ToggleTwoChoices = () => {
+  const [basicChoiceOn, setBasicChoiceOn] = useState<boolean>(true);
+
+  // Create handler function to pass as prop (to change state in this component)
+  const toggleChoice = () => {
+    setBasicChoiceOn((prev) => !prev);
+  };
+
+  return (
+    <div className="card">
+      <h2>Toggle two choices!</h2>
+
+      {/* Pass toggle function to child */}
+      <ToggleWithProps
+        buttonText="Toggle two choices"
+        toggleFunction={toggleChoice}
+      />
+
+      {/* Show component based on which is toggled */}
+      {basicChoiceOn ? (
+        <ComponentMappingWithTypes />
+      ) : (
+        <ComponentMappingWithInterfaceAndArray />
+      )}
+    </div>
+  );
+};
+
+export default ToggleTwoChoices;
