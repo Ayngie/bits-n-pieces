9d6fa206b8204b9886cd1086ca441ff2b066fb1e
Ayngie <angelicareutersward@gmail.com>
2025-03-26 13:43:01 +0100

    Feat: added types prop to button + default value

--- src/components/Buttons/ButtonWithProps.tsx
+++ src/components/Buttons/ButtonWithProps.tsx
@@ -1,13 +1,19 @@
 interface ButtonWithPropsProps {
   buttonText: string;
+  type?: 'button' | 'reset' | 'submit';
   toggleFunction?: () => void; //Accept the function as a prop
 }
 
 const ButtonWithProps: React.FC<ButtonWithPropsProps> = ({
   buttonText,
+  type = 'button', //default value
   toggleFunction,
 }) => {
-  return <button onClick={toggleFunction}>{buttonText}</button>;
+  return (
+    <button type={type} onClick={toggleFunction}>
+      {buttonText}
+    </button>
+  );
 };
 
 export default ButtonWithProps;
