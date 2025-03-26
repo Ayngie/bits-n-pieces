c434e0a97691f0902024cdc7f70e4886586db52b
Ayngie <angelicareutersward@gmail.com>
2025-03-26 12:43:29 +0100

    Feat: added sanitized input

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,5 +1,6 @@
 import ButtonWithProps from './Buttons/ButtonWithProps';
 import Counter from './Buttons/Counter';
+import ChangeButtonText from './Inputs/ChangeButtonText';
 
 const Buttons = () => {
   return (
@@ -7,11 +8,14 @@ const Buttons = () => {
       <h2>Buttons:</h2>
 
       <Counter />
+
       {/* Wrapping here for example component, to keep component file clean */}
       <article className="bordered-section">
         <h3>Button with props:</h3>
         <ButtonWithProps buttonText="Buttontext goes here" />
       </article>
+
+      <ChangeButtonText />
     </section>
   );
 };
