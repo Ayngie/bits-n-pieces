622d1b083f63bc0538129d3b29405ac273b3b475
Ayngie <angelicareutersward@gmail.com>
2025-04-01 13:34:04 +0200

    Feat: added functionality to radiobutton toggling style themes

--- src/components/Inputs/InputRadiobutton.tsx
+++ src/components/Inputs/InputRadiobutton.tsx
@@ -1,20 +1,51 @@
+import { useState } from 'react';
+
 const InputRadiobutton = () => {
+  const [theme, setTheme] = useState<string>('');
+
   return (
-    <article className="example-container">
+    <article className={`example-container ${theme}`}>
       <fieldset>
         <legend>
           <h2>Radiobutton input:</h2>
         </legend>
 
-        <p>Choose theme: </p>
+        <h3>Choose theme: </h3>
         <div>
           <label htmlFor="light">
-            <input id="light" type="radio" name="theme" value="option1" /> Light
-            mode
+            <input
+              id="light"
+              type="radio"
+              name="theme"
+              value="light-mode"
+              checked={theme === 'light-mode'}
+              onChange={(e) => setTheme(e.target.value)}
+            />{' '}
+            Light mode
           </label>
+
           <label htmlFor="dark">
-            <input id="dark" type="radio" name="theme" value="option2" /> Dark
-            mode
+            <input
+              id="dark"
+              type="radio"
+              name="theme"
+              value="dark-mode"
+              checked={theme === 'dark-mode'}
+              onChange={(e) => setTheme(e.target.value)}
+            />{' '}
+            Dark mode
+          </label>
+
+          <label htmlFor="neutral">
+            <input
+              id="neutral"
+              type="radio"
+              name="theme"
+              value=""
+              checked={theme === ''}
+              onChange={(e) => setTheme(e.target.value)}
+            />{' '}
+            None
           </label>
         </div>
       </fieldset>
